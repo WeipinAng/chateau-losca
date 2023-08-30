b95b0871758e6d908abc6cfae87be4d9683f19f2
Weipin Ang <80883634+WeipinAng@users.noreply.github.com>
2023-08-30 17:54:55 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,51 @@
+// Make mobile navigation work
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+    headerEl.classList.toggle("nav-open");
+});
+
+// Sticky navigation
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+    function (entries) {
+        const ent = entries[0];
+        console.log(ent);
+
+        if (ent.isIntersecting === false) {
+            document.body.classList.add("sticky");
+        }
+
+        if (ent.isIntersecting === true) {
+            document.body.classList.remove("sticky");
+        }
+    },
+    {
+        // In the viewport
+        root: null,
+        threshold: 0,
+        rootMargin: "-80px",
+    }
+);
+obs.observe(sectionHeroEl);
+
+document.onkeydown = function (e) {
+    if (e.keyCode == 123) {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == "I".charCodeAt(0)) {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == "J".charCodeAt(0)) {
+        return false;
+    }
+    if (e.ctrlKey && e.keyCode == "U".charCodeAt(0)) {
+        return false;
+    }
+
+    if (e.ctrlKey && e.shiftKey && e.keyCode == "C".charCodeAt(0)) {
+        return false;
+    }
+};
